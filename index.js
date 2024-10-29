c46f0811d8a1c2d277d63e9a37b826686e6a0ed6
fares_mohamed <113051677+fares-code@users.noreply.github.com>
2024-10-29 23:29:08 +0300

    add backend project

--- index.js
+++ index.js
@@ -8,23 +8,53 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
+// serve static files from the public directory
 app.use(express.static('public'));
 
-// http://expressjs.com/en/starter/basic-routing.html
+// routing for the home page
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-
-// your first API endpoint... 
+// your first API endpoint
 app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  res.json({ greeting: 'hello API' });
 });
 
+// current date/time API
+app.get('/api', (req, res) => {
+  res.json({
+    unix: Date.now(),
+    utc: new Date().toUTCString()
+  });
+});
 
+// timestamp API
+app.get('/api/:timestamp', (req, res) => {
+  const timestamp = req.params.timestamp;
+
+  // Check if the timestamp is a number and has a length of 13 (milliseconds)
+  if (!isNaN(timestamp) && timestamp.length === 13) {
+    return res.json({
+      unix: Number(timestamp),
+      utc: new Date(Number(timestamp)).toUTCString()
+    });
+  }
+
+  // Check if the timestamp can be parsed into a valid date
+  const date = new Date(timestamp);
+  if (date.toString() !== "Invalid Date") {
+    return res.json({
+      unix: date.getTime(),
+      utc: date.toUTCString()
+    });
+  }
+
+  // If none of the above conditions were met, respond with an error
+  res.json({ error: "Invalid Date" });
+});
 
 // Listen on port set in environment variable or default to 3000
 var listener = app.listen(process.env.PORT || 3000, function () {
